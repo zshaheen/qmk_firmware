9fe0a1486a5506678b2f3ba309d20831392fdf03
zshaheen <zshaheen93@gmail.com>
2023-01-11 00:31:18 -0800

    Use fcn keys in windows and add support to change brightness.

--- keyboards/keychron/k3_pro/ansi/rgb/keymaps/zshaheen/keymap.c
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/zshaheen/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                TO(L0),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [L3] = LAYOUT_ansi_84(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_TOG,
+     KC_ESC,   KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  RGB_TOG,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
@@ -59,56 +59,67 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 TO(L0), MO(L4),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [L4] = LAYOUT_ansi_84(
-     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  RGB_TOG,
-     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                TO(L0),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_TOG,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 TO(L0), MO(L4),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 };
+
+// This is a function I defined to be able to modify the brightness of the keys.
+void rgb_matrix_set_color_helper(uint8_t led_i, uint8_t hue, uint8_t sat, uint8_t val, uint8_t led_min, uint8_t led_max) {
+    HSV hsv = {hue, sat, val};
+    if (hsv.v > rgb_matrix_get_val()) {
+      hsv.v = rgb_matrix_get_val();
+    }
+
+    RGB rgb = hsv_to_rgb(hsv);
+    RGB_MATRIX_INDICATOR_SET_COLOR(led_i, rgb.r, rgb.g, rgb.b);
+}
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     for (uint8_t i = led_min; i < led_max; i++) {
         switch(get_highest_layer(layer_state|default_layer_state)) {
             case 4:
                 if (i == 78) {
-                  rgb_matrix_set_color(i, RGB_RED);
+                  rgb_matrix_set_color_helper(i, HSV_RED, led_min, led_max);
                 } else {
-                  rgb_matrix_set_color(i, RGB_PURPLE);
+                  rgb_matrix_set_color_helper(i, HSV_PURPLE, led_min, led_max);
                 }
                 break;
             case 3:
                 if (i == 78) {
-                  rgb_matrix_set_color(i, RGB_RED);
+                  rgb_matrix_set_color_helper(i, HSV_RED, led_min, led_max);
                 } else {
-                  rgb_matrix_set_color(i, RGB_CYAN);
+                  rgb_matrix_set_color_helper(i, HSV_CYAN, led_min, led_max);
                 }
                 break;
             case 2:
                 if (i == 78) {
-                  rgb_matrix_set_color(i, RGB_RED);
+                  rgb_matrix_set_color_helper(i, HSV_RED, led_min, led_max);
                 } else {
-                  rgb_matrix_set_color(i, RGB_GOLD);
+                  rgb_matrix_set_color_helper(i, HSV_GOLD, led_min, led_max);
                 }
                 break;
             case 1:
                 if (i == 78) {
-                  rgb_matrix_set_color(i, RGB_RED);
+                  rgb_matrix_set_color_helper(i, HSV_RED, led_min, led_max);
                 } else {
-                  rgb_matrix_set_color(i, RGB_WHITE);
+                  rgb_matrix_set_color_helper(i, HSV_WHITE, led_min, led_max);
                 }
                 break;
             case 0:
                  if (i == 15 || i == 30 || i == 45) {
-                    rgb_matrix_set_color(i, RGB_WHITE);
+                    rgb_matrix_set_color_helper(i, HSV_WHITE, led_min, led_max);
                  }
                  else if (17 <= i && i <= 20) {
-                    rgb_matrix_set_color(17, RGB_WHITE);
-                    rgb_matrix_set_color(18, RGB_GOLD);
-                    rgb_matrix_set_color(19, RGB_CYAN);
-                    rgb_matrix_set_color(20, RGB_PURPLE);
+                    rgb_matrix_set_color_helper(17, HSV_WHITE, led_min, led_max);
+                    rgb_matrix_set_color_helper(18, HSV_GOLD, led_min, led_max);
+                    rgb_matrix_set_color_helper(19, HSV_CYAN, led_min, led_max);
+                    rgb_matrix_set_color_helper(20, HSV_PURPLE, led_min, led_max);
                  } else {
-                    rgb_matrix_set_color(i, RGB_OFF);
+                    rgb_matrix_set_color_helper(i, HSV_OFF, led_min, led_max);
                  }
                  break;
             default:
