d0fb5ebb9eb2fc1215533fbf10630ec64ed14cc7
zshaheen <zshaheen93@gmail.com>
2023-03-14 21:54:52 -0700

    Add mouse key toggle key and swap fn keys on WIN_BASE and WIN_FN.

--- keyboards/keychron/q11/q11_ansi_stm32l432_ec11/keymaps/zshaheen/keymap.c
+++ keyboards/keychron/q11/q11_ansi_stm32l432_ec11/keymaps/zshaheen/keymap.c
@@ -25,6 +25,10 @@ enum layers{
     MOUSE_KEYS
 };
 
+enum keyboard_keycodes {
+  MK_WH_TOGGLE = SAFE_RANGE
+};
+
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
@@ -38,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(MAC_FN),  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         TO(WIN_BASE),  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         TO(WIN_FN),  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        TO(MOUSE_KEYS),  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_SPC,             KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        TO(MOUSE_KEYS),  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_SPC,             MK_WH_TOGGLE,  KC_RCMD, KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_91(
         RGB_TOG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  RGB_TOG,
@@ -49,15 +53,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(MOUSE_KEYS),  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_ansi_91(
-        KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
+        KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,    RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
         TO(MAC_BASE),  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         TO(MAC_FN),  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         TO(WIN_BASE),  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         TO(WIN_FN),  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        TO(MOUSE_KEYS),  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(WIN_FN),         KC_SPC,                        KC_SPC,             KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        TO(MOUSE_KEYS),  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(WIN_FN),         KC_SPC,                        KC_SPC,             MK_WH_TOGGLE,  KC_RALT, KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_ansi_91(
-        RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
+        RGB_TOG,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
         TO(MAC_BASE),  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         TO(MAC_FN),  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         TO(WIN_BASE),  _______,  _______, _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,             _______,            _______,
@@ -69,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(MAC_FN),  KC_TAB,  KC_BTN1,  KC_MS_U,  KC_BTN2,  XXXXXXX,  XXXXXXX,   XXXXXXX,  KC_BTN1,  KC_WH_U,  KC_BTN2,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,            KC_PGDN,
         TO(WIN_BASE),  KC_CAPS,  KC_MS_L, KC_MS_D, KC_MS_R,  XXXXXXX,  XXXXXXX,   XXXXXXX,  KC_WH_L,  KC_WH_D,  KC_WH_R,  XXXXXXX,  XXXXXXX, KC_ENT,             KC_HOME,
         TO(WIN_FN),  KC_LSFT, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,              KC_RSFT,  KC_UP,
-        TO(MOUSE_KEYS),  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(WIN_FN),            KC_SPC,                       KC_SPC,            KC_RCMD,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        TO(MOUSE_KEYS),  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(WIN_FN),            KC_SPC,                       KC_SPC,            MK_WH_TOGGLE, KC_RCMD, KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -81,6 +85,25 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MOUSE_KEYS]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif // ENCODER_MAP_ENABLE
+
+void ToggleMKWHLayer(void) {
+  if (IS_LAYER_ON(MOUSE_KEYS)) {
+    layer_off(MOUSE_KEYS);
+  } else if (IS_LAYER_OFF(MOUSE_KEYS)) {
+    layer_on(MOUSE_KEYS);
+  }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case MK_WH_TOGGLE:
+            if (record->event.pressed) {
+                ToggleMKWHLayer();
+            }
+            return false;
+    }
+    return true;
+};
 
 // This is a function I defined to be able to modify the brightness of the keys.
 void rgb_matrix_set_color_helper(uint8_t led_i, uint8_t hue, uint8_t sat, uint8_t val, uint8_t led_min, uint8_t led_max) {
@@ -105,19 +128,24 @@ bool linear_search(uint8_t key, uint8_t array[], int size) {
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     for (uint8_t i = led_min; i < led_max; i++) {
+        if (i == 83) {  // Right-hand red key.
+            rgb_matrix_set_color_helper(i, HSV_RED, led_min, led_max);
+            continue;
+        }
+
       layer_state_t highest_layer = get_highest_layer(layer_state|default_layer_state);
         switch(highest_layer) {
             case MAC_BASE:
                 rgb_matrix_set_color_helper(i, HSV_WHITE, led_min, led_max);
                 break;
             case MAC_FN:
-                rgb_matrix_set_color_helper(i, HSV_RED, led_min, led_max);
+                rgb_matrix_set_color_helper(i, HSV_CORAL, led_min, led_max);
                 break;
             case WIN_BASE:
                 rgb_matrix_set_color_helper(i, HSV_CYAN, led_min, led_max);
                 break;
             case WIN_FN:
-                rgb_matrix_set_color_helper(i, HSV_GREEN, led_min, led_max);
+                rgb_matrix_set_color_helper(i, HSV_TURQUOISE, led_min, led_max);
                 break;
             case MOUSE_KEYS:
                 if (linear_search(i, other_keys, sizeof(other_keys)/sizeof(other_keys[0]))) {
